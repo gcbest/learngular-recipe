147b621b0cdf19e946c57d99ff2257e53afc6cb7
Godfrey Best <gcb246@stern.nyu.edu>
2017-06-15 13:02:00 -0400

    models added

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+export class Recipe {
+  // makes it accessible outside this script
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  // only an array of Recipe objects will be stored in recipes
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Simpy a test', 'https://s-media-cache-ak0.pinimg.com/originals/1e/94/b8/1e94b87644df699f0d24c1b20e71e7f7.jpg'),
+    new Recipe('Test Recipe', 'Simpy a test', 'https://s-media-cache-ak0.pinimg.com/originals/1e/94/b8/1e94b87644df699f0d24c1b20e71e7f7.jpg')
+  ];
 
   constructor() { }
 
