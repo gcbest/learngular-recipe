fb75f9bc9ccb245b67e83a0646849eeb3d1df816
Godfrey Best <gcb246@stern.nyu.edu>
2017-06-16 11:58:29 -0400

    header updated and details linked

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -11,12 +11,18 @@ export class RecipesListComponent implements OnInit {
   // only an array of Recipe objects will be stored in recipes
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'Simpy a test', 'https://s-media-cache-ak0.pinimg.com/originals/1e/94/b8/1e94b87644df699f0d24c1b20e71e7f7.jpg'),
-    new Recipe('Test Recipe', 'Simpy a test', 'https://s-media-cache-ak0.pinimg.com/originals/1e/94/b8/1e94b87644df699f0d24c1b20e71e7f7.jpg')
+    new Recipe('A Next Test Recipe', 'Simpy a test', 'https://s-media-cache-ak0.pinimg.com/originals/1e/94/b8/1e94b87644df699f0d24c1b20e71e7f7.jpg')
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
